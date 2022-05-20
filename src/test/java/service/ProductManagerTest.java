8d705dbdac68060f5f49dd551732224102dc17d2
TurskiPavel <PTurskiy@alfabank.ru>
2022-05-20 11:03:20 +0300

    @BeforeEach @AfterEach

--- src/test/java/service/ProductManagerTest.java
+++ src/test/java/service/ProductManagerTest.java
@@ -1,15 +1,10 @@
 package service;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import ru.clevertec.dto.Order;
 import ru.clevertec.dto.Product;
-import ru.clevertec.util.OrderUtil;
 import ru.clevertec.util.ProductUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ProductManagerTest {
@@ -17,7 +12,6 @@ public class ProductManagerTest {
     // Задача: Написать метод @BeforeEach & AfterEach, которые будут создавать и уничтожать список заказов
 
     static List<Product> products;
-    List<Order> orders;
 
     @BeforeAll
     static void createProducts() {
@@ -28,15 +22,4 @@ public class ProductManagerTest {
     static void deleteProducts() {
         products = null;
     }
-
-    @BeforeEach
-    void createOrders() {
-        orders = OrderUtil.createOrders();
-    }
-
-    @AfterEach
-    void deleteOrders() {
-        orders = null;
-    }
-
 }

b97109a05c5aa54c2c3f369453067ea777b1f53c
TurskiPavel <PTurskiy@alfabank.ru>
2022-05-20 10:58:56 +0300

    @BeforeAll @AfterAll

--- src/test/java/service/ProductManagerTest.java
+++ src/test/java/service/ProductManagerTest.java
@@ -1,27 +1,7 @@
 package service;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import ru.clevertec.dto.Product;
-import ru.clevertec.util.ProductUtils;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class ProductManagerTest {
 
     // Задача: Написать метод @BeforeAll & AfterAll, которые будут создавать и уничтожать список продуктов
-
-    static List<Product> products = new ArrayList<>();
-
-    @BeforeAll
-    static void createProducts() {
-        products = ProductUtils.createProducts();
-    }
-
-    @AfterAll
-    static void deleteProducts() {
-        products = null;
-    }
 
 }

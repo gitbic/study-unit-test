bcd0884a2ebe119ced148783b1970456e3efc5a2
TurskiPavel <PTurskiy@alfabank.ru>
2022-05-20 16:20:52 +0300

    findProductTest

--- src/test/java/service/ProductManagerTest.java
+++ src/test/java/service/ProductManagerTest.java
@@ -2,20 +2,14 @@ package service;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import ru.clevertec.dto.MetaInf;
 import ru.clevertec.dto.Order;
 import ru.clevertec.dto.Product;
-import ru.clevertec.dto.Shop;
-import ru.clevertec.exceptions.ProductException;
 import ru.clevertec.service.ProductManager;
 import ru.clevertec.util.OrderUtil;
 import ru.clevertec.util.ProductUtils;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public class ProductManagerTest {
@@ -47,30 +41,4 @@ public class ProductManagerTest {
 //    Задача: Написать позитивные и негативные тесты для метода getProductByName
 //    Задача: Написать позитивные и негативные тесты для метода getProductByName
 
-    @Test
-    void getProductByNameTest() {
-        Product expectedApple = new Product()
-            .setName("apple")
-            .setPrice(1.24)
-            .setWeight(500)
-            .setMetaInf(new MetaInf()
-                .setDiscount(false)
-                .setExpirationDate(LocalDate.parse("2022-06-03"))
-                .setShopList(List.of(
-                    Shop.EVROOPT,
-                    Shop.DVA_GUSIA,
-                    Shop.KOPEECHKA
-                ))
-            );
-
-        Product apple = productManager.getProductByName(products, "apple");
-
-        Assertions.assertEquals(expectedApple, apple);
-    }
-
-    @Test
-    void getProductByNameFailedTest() {
-        Assertions.assertThrows(ProductException.class,
-            () -> productManager.getProductByName(products, "yabloko"));
-    }
 }

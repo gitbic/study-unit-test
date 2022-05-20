29fe07dd8d607cb1a1c4a5a3f7fe92193d9bf306
TurskiPavel <PTurskiy@alfabank.ru>
2022-05-20 15:22:27 +0300

    findProductTest

--- src/test/java/service/ProductManagerTest.java
+++ src/test/java/service/ProductManagerTest.java
@@ -2,21 +2,27 @@ package service;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.clevertec.dto.MetaInf;
 import ru.clevertec.dto.Order;
 import ru.clevertec.dto.Product;
+import ru.clevertec.dto.Shop;
+import ru.clevertec.exceptions.ProductException;
+import ru.clevertec.service.ProductManager;
 import ru.clevertec.util.OrderUtil;
 import ru.clevertec.util.ProductUtils;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class ProductManagerTest {
 
-    // Задача: Написать метод @BeforeEach & AfterEach, которые будут создавать и уничтожать список заказов
-
     static List<Product> products;
     List<Order> orders;
+    ProductManager productManager = new ProductManager();
 
     @BeforeAll
     static void createProducts() {
@@ -38,4 +44,33 @@ public class ProductManagerTest {
         orders = null;
     }
 
+//    Задача: Написать позитивные и негативные тесты для метода getProductByName
+//    Задача: Написать позитивные и негативные тесты для метода getProductByName
+
+    @Test
+    void getProductByNameTest() {
+        Product expectedApple = new Product()
+            .setName("apple")
+            .setPrice(1.24)
+            .setWeight(500)
+            .setMetaInf(new MetaInf()
+                .setDiscount(false)
+                .setExpirationDate(LocalDate.parse("2022-06-03"))
+                .setShopList(List.of(
+                    Shop.EVROOPT,
+                    Shop.DVA_GUSIA,
+                    Shop.KOPEECHKA
+                ))
+            );
+
+        Product apple = productManager.getProductByName(products, "apple");
+
+        Assertions.assertEquals(expectedApple, apple);
+    }
+
+    @Test
+    void getProductByNameFailedTest() {
+        Assertions.assertThrows(ProductException.class,
+            () -> productManager.getProductByName(products, "yabloko"));
+    }
 }
